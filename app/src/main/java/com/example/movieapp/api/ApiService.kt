acb609eeafd495c1711d9101242d9eb6f7ab3990
mo samir <smyr03130@gmail.com>
2023-02-27 17:14:23 +0200

    Version 1

---
+++ app/src/main/java/com/example/movieapp/api/ApiService.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp.api
+
+import com.example.movieapp.models.TvShowResponse
+import com.example.movieapp.util.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET(Constants.END_POINT)
+    suspend fun getTvShows(): Response<TvShowResponse>
+
+}
---
+++ app/src/main/java/com/example/movieapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.util
+
+object Constants {
+
+    const val BASE_URL = "https://api.tvmaze.com/"
+    const val END_POINT = "shows"
+
+}
